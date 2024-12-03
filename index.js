0a0a1e18be4d75c2864e49d02d3ce661bfed5977
edith.githinji <edithgithinji2020@gmail.com>
2024-12-03 12:11:17 +0300

    concantation using the + operator

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Edith Githinji';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
